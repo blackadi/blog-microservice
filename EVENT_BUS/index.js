86087fda365154b51aa894813e29fbf31b9705ea
Odai Shalabi <odai.shalabi@hotmail.com>
2021-01-03 16:49:12 +0200

    broker

--- EVENT_BUS/index.js
+++ EVENT_BUS/index.js
@@ -8,8 +8,13 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+//create a data structur to store all the events recevied so if a service went down it can reclam the event again ...... this is a POC code not a production ready app
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event); //send a copy to the post service
   axios.post("http://localhost:4001/events", event); //send a copy to the comment service
@@ -17,6 +22,11 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4003/events", event); //send a copy to the moderation service
 
   res.send({ status: "OK" });
+});
+
+//provide all the stored events inside the broker
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # blog-microservice
+
+a sample POC on microservice application to explain in a very naive way how microservice  works under the hood.
+
+Posts service runs on port 4000
+Comments service runs on port 4001
+Query service runs on port 4002
+Moderation service runs on port 4003
+
+All those services emits an events into the event bus (BROKER) which is running on port 4005
