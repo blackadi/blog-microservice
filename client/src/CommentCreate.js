ebfbd4266ce4b89cecee1e338e945d5650365a87
Addi <odai.shalabi@gmail.com>
2021-03-16 19:00:35 +0200

    k8s added for testing

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -7,7 +7,7 @@ const CommentCreate = ({ postId }) => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    //Im connecting to ks8 using ngix-ingress srv, CHANGE THIS TO localhost:4001 if you are not using ks8
+    // K8s is being used with ngix-ingress srv, CHANGE THIS TO localhost:4001 otherwise
     await axios.post(`http://posts.com/posts/${postId}/comments`, {
       content,
     });
